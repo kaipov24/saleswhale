25cb5749820adbb4f1f3d49fa981965fa0f52c6e
Kayrat <kaipov.kayrat@gmail.com>
2020-08-11 01:58:24 +0600

    css conflict

--- client/html.js
+++ client/html.js
@@ -4,7 +4,6 @@ const Html = ({ body }) => {
   <html>
     <head>
       <meta charset="UTF-8">
-      <link rel="stylesheet" type="text/css" href="/css/main.css" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
     </head>
     <body>
