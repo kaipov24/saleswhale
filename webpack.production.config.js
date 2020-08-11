770bc19f2c40514dceb627ff9fc45a7eda9ae7d0
Kayrat Kaipov <59024495+kaipov24@users.noreply.github.com>
2020-08-11 13:48:37 +0600

    entry root deleted

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -27,8 +27,8 @@ const config = {
     ]
   },
   entry: {
-    main: './main.js',
-    root: './config/root.js'
+    main: './main.js'
+    // root: './config/root.js'
   }, // [('./main.js', './config/root.js')],
   resolve: {
     alias: {
